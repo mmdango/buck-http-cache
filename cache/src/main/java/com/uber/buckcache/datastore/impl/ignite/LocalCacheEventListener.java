43f8ae3141fd4a57cea6697a6a5b730b7819f5b5
Michael Dang <michael.dang@airbnb.com>
2019-05-03 13:44:27 -0700

    Adds eviction rate to statsd

--- cache/src/main/java/com/uber/buckcache/datastore/impl/ignite/LocalCacheEventListener.java
+++ cache/src/main/java/com/uber/buckcache/datastore/impl/ignite/LocalCacheEventListener.java
@@ -1,13 +1,15 @@
 package com.uber.buckcache.datastore.impl.ignite;
 
-import static com.uber.buckcache.datastore.impl.ignite.IgniteConstants.ARTIFACT_CACHE_NAME;
-import static com.uber.buckcache.datastore.impl.ignite.IgniteConstants.EVENT_TYPE_TO_NAME_MAP;
-
+import com.uber.buckcache.utils.StatsDClient;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.events.CacheEvent;
 import org.apache.ignite.lang.IgnitePredicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.uber.buckcache.datastore.impl.ignite.IgniteConstants.ARTIFACT_CACHE_NAME;
+import static com.uber.buckcache.datastore.impl.ignite.IgniteConstants.EVENT_TYPE_TO_NAME_MAP;
+import static com.uber.buckcache.utils.MetricsRegistry.ARTIFACT_EVICTION_COUNT;
 
 public class LocalCacheEventListener implements IgnitePredicate<CacheEvent> {
   private static Logger logger = LoggerFactory.getLogger(LocalCacheEventListener.class);
@@ -39,6 +41,8 @@ public class LocalCacheEventListener implements IgnitePredicate<CacheEvent> {
       }
 
       metadataCache.remove(underlyingKey);
+
+      StatsDClient.get().count(ARTIFACT_EVICTION_COUNT, 1L);
     }
 
     return true;
